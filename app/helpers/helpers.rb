075cd894792bdab85c2d2496ed380b83ea0c5938
David Slivken <slivken@gmail.com>
2017-02-12 14:19:27 +0000

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,12 @@
 class Helpers
+
+  def self.current_user(session)
+    puts session
+    User.find(session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    session.include?(:user_id)
+  end
+
 end
